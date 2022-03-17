c80c1a46266f8ffd79ee545eb57bbc8037bf11b2
MateoJacomeUPC <mateo.jacome@estudiantat.upc.edu>
2022-03-17 18:01:24 +0100

    Modifying data_collection.py.

--- data_collection.py
+++ data_collection.py
@@ -2,11 +2,16 @@ from hdfs import InsecureClient
 
 client = InsecureClient('http://10.4.41.72:9870', user='bdm')
 
-# #Writing part of a file.
-# with open('Data/opendatabcn-income/2017_Distribució_territorial_renda_familiar.csv', encoding='UTF-8') as reader, \
-#         client.write('test_2017_rendaaa.csv', encoding= 'UTF-8') as writer:
-#     for line in reader:
-#         writer.write(line)
+################# Testing stuff
+#Writing part of a file.
+with open('Data/opendatabcn-income/2017_Distribució_territorial_renda_familiar.csv', encoding='UTF-8') as reader, \
+        client.write('foldertest/test_2018_renda.csv', encoding= 'UTF-8') as writer:
+    for line in reader:
+        writer.write(line)
 
-with client.read('test_2017_rendaaa.csv', encoding = 'UTF-8') as reader:
+with client.read('foldertest/test_2018_renda.csv', encoding = 'UTF-8') as reader:
     print(reader.read())
+#################
+
+### Upload files
+
