487c234d348ebd02e765d0440692351f0a5124d1
Kat W <73595864+KatBCN@users.noreply.github.com>
2022-04-04 22:46:05 +0200

    cleaning up old metadata

--- data_persistence_loader_kat.py
+++ data_persistence_loader_kat.py
@@ -322,6 +322,7 @@ def writeParquetFile(source, table):
                    row_group_size=134217728) #128 mb
 
 clean_directory_of_files_ending_in('landing_persistent/opendatabcn-income/', '.parquet')    
+clean_directory_of_files_ending_in('landing_persistent/opendatabcn-income/', '_metadata')  
 
 hdfs_path = "hdfs://meowth.fib.upc.es:27000/user/bdm"
 directory = "landing_temporal"
